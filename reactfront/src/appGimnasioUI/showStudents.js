578a180d9004b5114f59e0bf3a55a12f5fe9e85c
AgustinEGarcia <agustinezequiel1997@gmail.com>
2022-04-26 14:28:41 -0300

    desarrollo de componente para mostrar todos los alumnos

---
+++ reactfront/src/appGimnasioUI/showStudents.js
@@ -0,0 +1,86 @@
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+
+//constante para hacer referencia a nuestro endpoint
+const URI = 'http://localhost:8000/students/'
+
+//componente funcional
+const CompShowStudents = () => {
+    const [students, setStudent] = useState([])
+    
+    useEffect(()=>{
+        getStudents()
+    },[])
+
+    //procedimiento para mostrar todos los estudiantes
+    const getStudents = async () => {
+        const res = await axios.get(URI)
+        setStudent(res.data)
+    }
+
+//procedimiento para eliminar un estudiante
+    const deleteStudent = async (id) => {
+        axios.delete(`${URI}${id}`)
+        getStudents()
+    }
+
+    return(
+        <div className='container'>
+            
+            <div className='row'>
+
+                <div className='col'>
+                    
+                    <table className="table table-hover">
+                        
+                        <thead className='´table-primary'>
+
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>DNI</th>
+                                <th>Fecha de Nacimiento</th>
+                                <th>Nro. de teléfono</th>
+                                <th>Tipo de membresía</th>
+                                <th>Turno asignado</th>
+                                <th>Estado de membresía</th>
+                                <th>Acciones</th>
+                            </tr>
+
+                        </thead>
+
+                        <tbody>
+                            {students.map ((student) => (
+                                <tr key={student.id}>
+                                    <td>{student.ID}</td>
+                                    <td>{student.Nombre}</td>
+                                    <td>{student.Apellido}</td>
+                                    <td>{student.DNI}</td>
+                                    <td>{student.Fecha_nacimiento}</td>
+                                    <td>{student.Nro_telefono}</td>
+                                    <td>{student.Tipo_membresia}</td>
+                                    <td>{student.Turno_asignado}</td>
+                                    <td>{student.Estado_membresia}</td>
+                                    <td>
+                                        {/* <Link to={`/edit/${student.id}`} className='btn btn-info'><i class="fa-solid fa-pen-to-square"></i></Link> */}
+                                        <button onClick={ () => deleteStudent(student.id)} className='btn btn_danger'><i class="fa-solid fa-circle-trash"></i></button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+
+                    </table>
+
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+
+
+export default CompShowStudents
