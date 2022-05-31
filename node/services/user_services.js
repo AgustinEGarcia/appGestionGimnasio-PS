e5064ec804f60a8cad7ebeea78da0c7b7a7ebb9a
AgustinEGarcia <agustinezequiel1997@gmail.com>
2022-05-30 22:44:00 -0300

    fix delete turno asignado

--- node/services/user_services.js
+++ node/services/user_services.js
@@ -1,29 +1,38 @@
 import AppModel from "../models/AppModel.js";
 
-async function getAllStudents(){
-    return await AppModel.findAll()
+async function getAllStudents() {
+  return await AppModel.findAll();
 }
 
-async function getStudentByID(id){
-    return await AppModel.findAll({
-        where:{id}
-    })
+async function getStudentByID(id) {
+  return await AppModel.findAll({
+    where: { id },
+  });
 }
 
-async function createStudent(req){
-   return await AppModel.create(req.body)
+async function createStudent(req) {
+  return await AppModel.create(req.body);
 }
 
-async function updateStudent(req){
-    return await AppModel.update(req.body, {
-        where: {id: req.params.id}
-    })
+async function updateStudent(req) {
+  return await AppModel.update(
+    { ...req.body, Turno_asignado: req.body.Turno_asignado || null },
+    {
+      where: { id: req.params.id },
+    }
+  );
 }
 
-async function deleteStudent(req){
-    return await AppModel.destroy({
-        where: {id: req.params.id}
-    })
+async function deleteStudent(req) {
+  return await AppModel.destroy({
+    where: { id: req.params.id },
+  });
 }
 
-export default {getAllStudents, getStudentByID, createStudent,updateStudent, deleteStudent}
+export default {
+  getAllStudents,
+  getStudentByID,
+  createStudent,
+  updateStudent,
+  deleteStudent,
+};
