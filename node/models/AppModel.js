a872a69b5707bd342edfd203092464c453839480
AgustinEGarcia <agustinezequiel1997@gmail.com>
2022-06-13 15:25:14 -0300

    fix models

--- node/models/AppModel.js
+++ node/models/AppModel.js
@@ -2,16 +2,19 @@ import db from "../database/db.js";
 
 import { DataTypes } from "sequelize";
 
-const AppModel = db.define('alumno', {
+const AppModel = db.define(
+  "alumno",
+  {
     Nombre: { type: DataTypes.STRING },
     Apellido: { type: DataTypes.STRING },
     DNI: { type: DataTypes.STRING },
     Fecha_nacimiento: { type: DataTypes.DATE },
     Nro_telefono: { type: DataTypes.STRING },
     Tipo_membresia: { type: DataTypes.STRING },
-    Turno_asignado: { type: DataTypes.INTEGER },
-    Estado_membresia: { type: DataTypes.STRING }
+    Turno_asignado: { type: DataTypes.STRING },
+    Estado_membresia: { type: DataTypes.STRING },
+  },
+  { freezeTableName: true, timestamps: false }
+);
 
-}, {freezeTableName: true, timestamps: false})
-
-export default AppModel
+export default AppModel;
